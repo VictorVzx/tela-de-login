24867157879bbd746321f5250813d3366e606d87
Victor Compertino <gifpa45we@gmail.com>
2025-11-24 00:48:15 -0300

    New addings to the login screen (v: 1.0)

---
+++ script.js
@@ -0,0 +1,14 @@
+function toggleSenha() {
+    const input = document.getElementById("senha");
+    const icon = document.querySelector(".toggleSenha");
+
+    if (input.type === "password") {
+        input.type = "text";
+        icon.classList.remove("fa-eye");
+        icon.classList.add("fa-eye-slash");
+    } else {
+        input.type = "password";
+        icon.classList.remove("fa-eye-slash");
+        icon.classList.add("fa-eye");
+    }
+}
