62520c46c6bd94cbbb176e4b0bc5752622aadf2b
Victor Compertino <gifpa45we@gmail.com>
2025-11-25 22:10:04 -0300

    Mudanças na porta de acesso e melhorias

--- server.js
+++ server.js
@@ -65,4 +65,5 @@ app.post("/login", async (req, res) => {
   });
 });
 
-app.listen(3000, () => console.log("Servidor rodando na porta 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
